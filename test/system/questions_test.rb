945484f25bb571bb3efa541212c712a7bdf774f0
Alice Laurin <laurin.alice@gmail.com>
2023-11-13 14:17:41 +0100

    ex 2

---
+++ test/system/questions_test.rb
@@ -0,0 +1,44 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+end
+
+class QuestionsTest < ApplicationSystemTestCase
+  # [...]
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+end
+
+class QuestionsTest < ApplicationSystemTestCase
+  # [...]
+
+  test "saying Why? yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Why?"
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
+  end
+end
+
+class QuestionsTest < ApplicationSystemTestCase
+  # [...]
+
+  test "saying I am going to work right now yields a nice response from the coach" do
+    visit ask_url
+    fill_in "question", with: "I am going to work right now!"
+    click_on "Ask"
+
+    assert_text "Great!"
+  end
+end
